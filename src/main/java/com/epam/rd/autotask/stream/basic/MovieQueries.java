d90d373f242f41a7ec997dc2411726e2d84281b1
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-11-03 21:51:17 +0100

    Ok

--- src/main/java/com/epam/rd/autotask/stream/basic/MovieQueries.java
+++ src/main/java/com/epam/rd/autotask/stream/basic/MovieQueries.java
@@ -1,33 +1,45 @@
 package com.epam.rd.autotask.stream.basic;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class MovieQueries {
+    List<String> movies;
 
     public MovieQueries(List<String> movies) {
+//        if (movies.isEmpty()) throw new IllegalArgumentException();
+        this.movies = movies;
     }
 
     public long getNumberOfMovies() {
-        return 0;
+        return movies.size();
     }
 
     public long getNumberOfMoviesThatStartsWith(String start) {
-        return 0;
+        return movies.stream()
+                .filter(m -> m.startsWith(start))
+                .count();
     }
 
     public long getNumberOfMoviesThatStartsWithAndEndsWith(String start, String end) {
-        return 0;
+        return movies.stream()
+                .filter(m -> m.startsWith(start) && m.endsWith(end))
+                .count();
     }
 
     public List<Integer> getLengthOfTitleOfMovies() {
-        return null;
+        return movies.stream()
+                .mapToInt(String::length)
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     public int getNumberOfLettersInShortestTitle() {
-        return 0;
+        Optional<String> min = movies.stream()
+                .min(Comparator.comparing(String::length));
+        return Math.toIntExact(min.stream().mapToInt(String::length).count());
     }
 
     public Optional<String> getFirstTitleThatContainsThreeWords() {
@@ -35,9 +47,12 @@ public class MovieQueries {
     }
 
     public List<String> getFirstFourTitlesThatContainAtLeastTwoWords() {
-        return null;
+        return movies.stream()
+                .filter(s -> s.length() > 2)
+                .collect(Collectors.toList());
     }
 
     public void printAllTitleToConsole() {
+        movies.forEach(System.out::println);
     }
 }
